5e41058c9e3c5ff923bc3a875e659507e3f4df51
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2025-01-18 18:58:26 +0100

    add

---
+++ work1.py
@@ -0,0 +1,34 @@
+from queue import Queue
+import random
+import time
+
+q = Queue()
+
+def generate_request():
+    num = random.randint(1, 100)
+    application = f"Заявка {num}"
+    q.put(application)
+    print(f"Нова заяква створена {application}")
+     
+def process_request():
+    if not q.empty():
+        curr_q = q.get()
+        time.sleep(1)
+        print(f"Обробляється заявка {curr_q}...")
+    else:
+        print("Черга пуста")
+            
+def main():
+    print("Система обробки заявок запущена. Натисніть Ctrl+C для завершення.")
+    try:
+        while True:
+            generate_request()
+            time.sleep(2)
+            process_request()       
+    except KeyboardInterrupt:
+        print("\n Система обробки заявок зупинена.")
+
+           
+if __name__ == "__main__":
+    main()        
+
